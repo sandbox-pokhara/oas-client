37a79b14190e67ddb3efad432f5d80ad90431350
leyuskckiran1510 <kdhakal1510@gmail.com>
2025-08-05 12:04:12 +0545

    refactor: use clean set initialization

--- oas_client/constants.py
+++ oas_client/constants.py
@@ -1,12 +1,10 @@
 from typing import Literal
 
-BASE_IMPORTS = set(
-    [
-        ("typing", "Any"),
-        ("typing", "Literal"),
-    ]
-)
+BASE_IMPORTS = {
+    ("typing", "Any"),
+    ("typing", "Literal"),
+}
 CONDITIONAL_IMPORTS: dict[Literal["pydantic", "typing"], set[tuple[str, str]]] = {
-    "pydantic": set([("pydantic", "BaseModel")]),
-    "typing": set([("typing_extensions", "TypedDict"), ("typing", "NotRequired")]),
+    "pydantic": {("pydantic", "BaseModel")},
+    "typing": {("typing_extensions", "TypedDict"), ("typing", "NotRequired")},
 }
